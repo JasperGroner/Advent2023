2551219dc20a9349cee70ea51652309a4ea32307
Jasper Groner <jasper.groner@rover.com>
2023-12-04 11:37:02 -0500

    update day 3 test name

--- day3/day3part2_test.py
+++ day3/day3part2_test.py
@@ -1,7 +1,7 @@
 import unittest
 from day3part2 import main
 
-class Day3Part1Test(unittest.TestCase):
+class Day3Part2Test(unittest.TestCase):
     def test_sample(self):
         result = main(['testinput.txt'])
         self.assertEqual(result, 467835)
