acb7a5363bbdca0781ae4def4e097ce83941d766
Jasper Groner <jasper.groner@rover.com>
2023-12-10 12:46:16 -0500

    day 10

---
+++ day10/day10part1.py
@@ -0,0 +1,121 @@
+import sys
+
+EXITS = {
+    "|": ["+y", "-y"],
+    "-": ["+x", "-x"],
+    "L": ["-y", "+x"],
+    "J": ["-y", "-x"],
+    "7": ["+y", "-x"],
+    "F": ["+y", "+x"],
+    ".": [],
+}
+
+class Pipe:
+    """
+        Stores openings from a given pipe.
+    """
+
+    def __init__(self, x: int, y: int, char: str):
+        self.prev_pipe = {}
+        self.next_pipe = {}
+        self.location = {"x": x, "y": y}
+        self.char = char
+
+    def set_prev_pipe(self,  prev_x: int, prev_y: int):
+        self.prev_pipe = {"x": prev_x, "y": prev_y}
+
+    def get_next_pipe(self):
+        pipe1, pipe2 = Pipe.get_adjoining_pipes(self.char, self.location["x"], self.location["y"])
+        self.next_pipe = pipe1 if pipe2 == self.prev_pipe else pipe2
+    
+    @staticmethod
+    def get_adjoining_pipes(char: str, x: int, y: int):
+        if (char == "." or char == "S"):
+            print(char)
+            return None
+        exits = EXITS[char]
+        [pipe1, pipe2] = [Pipe.translate_map_char(exit, x, y) for exit in exits]
+        return pipe1, pipe2
+
+    @staticmethod
+    def translate_map_char(char:str, x: int, y: int):
+        if char.endswith("x"):
+            if char.startswith("+"):
+                return {"x": x + 1, "y": y}
+            return {"x": x - 1, "y": y}
+        else:
+            if char.startswith("+"):
+                return {"x": x, "y": y + 1}
+            return {"x": x, "y": y - 1}
+
+        
+def make_line_array(input_file_name: str):
+    """
+        Take an input file and return
+        array of the lines in the file.
+    """
+
+    with open(input_file_name, 'r') as file_reader:
+        raw_input = file_reader.read()
+        line_array = raw_input.splitlines()
+
+        return line_array
+
+def find_start(line_array):
+    for y, line in enumerate(line_array):
+        for x, char in enumerate(line):
+            if char == 'S':
+                return y, x
+
+def get_first_steps(line_array, start_y, start_x):
+    start_location = {"y": start_y, "x": start_x}
+    first_steps = []
+    adjoining_locations = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    for location in adjoining_locations:
+        adjoining_locations = Pipe.get_adjoining_pipes(line_array[start_y + location[0]][start_x + location[1]], start_x + location[1], start_y + location[0])
+        if line_array[start_y + location[0]][start_x + location[1]] != '.' and \
+                start_location in adjoining_locations:
+            first_steps.append(Pipe(start_x + location[1], start_y + location[0], line_array[start_y + location[0]][start_x + location[1]]))
+
+    for pipe in first_steps:
+        pipe.set_prev_pipe(start_x, start_y)
+        pipe.get_next_pipe()
+    return first_steps
+
+def get_answer(line_array, pipes):
+    # start with one step, since we're one from start
+    answer = 1
+    while (pipes[0].location["x"] != pipes[1].location["x"] or pipes[0].location["y"] != pipes[1].location["y"]):
+        for idx, pipe in enumerate(pipes):
+            prev_x, prev_y = pipe.location["x"], pipe.location["y"]
+            pipes[idx] = Pipe(x = pipe.next_pipe["x"], y = pipe.next_pipe["y"],
+                char = line_array[pipe.next_pipe["y"]][pipe.next_pipe["x"]])
+            pipes[idx].set_prev_pipe(prev_x, prev_y)
+            pipes[idx].get_next_pipe()
+        answer += 1
+    # increment once more for final step
+    return answer
+
+def main(argv):
+    """
+        What do you think main does?
+    """
+    if len(argv) < 1:
+        print("Please include file input")
+        return
+
+    input_file_name = argv[0]
+
+    line_array = make_line_array(input_file_name)
+
+    start_y, start_x = find_start(line_array)
+
+    first_steps = get_first_steps(line_array, start_y, start_x)
+
+    answer = get_answer(line_array, first_steps)
+
+    print(answer)
+    return answer
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ day10/day10part2.py
@@ -0,0 +1,93 @@
+import sys
+
+from day10part1 import make_line_array, find_start, get_first_steps, EXITS, Pipe
+
+def get_start_char(first_steps):
+    openings = []
+    for pipe in first_steps:
+        if pipe.location["x"] - pipe.prev_pipe["x"] == 1:
+            openings.append("+x")
+        elif pipe.location["x"] - pipe.prev_pipe["x"] == -1:
+            openings.append("-x")
+        elif pipe.location["y"] - pipe.prev_pipe["y"] == 1:
+            openings.append("+y")
+        else:
+            openings.append("-y")
+    
+    for key, value in EXITS.items():
+        if value == openings:
+            return key
+
+def get_clean_line_array(line_array, pipes, start_y, start_x):
+    clean_line_array = []
+    for idx, line in enumerate(line_array):
+        clean_line_array.append([])
+        for char in line:
+            clean_line_array[idx].append(".")
+    
+    clean_line_array[start_y][start_x] = get_start_char(pipes)
+
+    while (pipes[0].location["x"] != pipes[1].location["x"] or pipes[0].location["y"] != pipes[1].location["y"]):
+        for idx, pipe in enumerate(pipes):
+            clean_line_array[pipe.location["y"]][pipe.location["x"]] = pipe.char
+            prev_x, prev_y = pipe.location["x"], pipe.location["y"]
+            pipes[idx] = Pipe(x = pipe.next_pipe["x"], y = pipe.next_pipe["y"],
+                char = line_array[pipe.next_pipe["y"]][pipe.next_pipe["x"]])
+            pipes[idx].set_prev_pipe(prev_x, prev_y)
+            pipes[idx].get_next_pipe()
+
+    clean_line_array[pipes[0].location["y"]][pipes[0].location["x"]] = pipes[0].char
+
+    return clean_line_array
+
+
+def is_interior(y, x, clean_line_array):
+    if clean_line_array[y][x] != '.':
+        return False
+    
+    left, right = 0, 0
+    for y_loc in range(y, -1, -1):
+        char = clean_line_array[y_loc][x]
+        if char in ["-", "L", "F"]:
+            right += 1
+        if char in ["-", "J", "7"]:
+            left += 1
+    
+    num_crossings = min([left, right])
+
+    return num_crossings % 2 == 1
+
+
+def get_number_of_interior_spaces(clean_line_array):
+    number = 0
+    for y, line in enumerate(clean_line_array):
+        for x, char in enumerate(line):
+            number += 1 if is_interior(y, x, clean_line_array) else 0
+    return number
+
+
+def main(argv):
+    """
+        What do you think main does?
+    """
+    if len(argv) < 1:
+        print("Please include file input")
+        return
+
+    input_file_name = argv[0]
+
+    line_array = make_line_array(input_file_name)
+
+    start_y, start_x = find_start(line_array)
+
+    first_steps = get_first_steps(line_array, start_y, start_x)
+
+    clean_line_array = get_clean_line_array(line_array, first_steps, start_y, start_x)
+
+    answer = get_number_of_interior_spaces(clean_line_array)
+    
+    print(answer)
+    return answer
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
